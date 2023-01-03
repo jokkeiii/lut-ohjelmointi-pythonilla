43af0a98474bbeec20f33d96852e8b80979fd99f
jokkeiii <joakim.ijas@gmail.com>
2023-01-03 10:03:20 +0200

    created t4 and t5

--- L02-tehtavat/L02T4.py
+++ L02-tehtavat/L02T4.py
@@ -3,4 +3,12 @@ PII = 3.14
 ekaLuku = int(input("Anna positiivinen kokonaisluku: "))
 print("Luku", ekaLuku, "kerrottuna itsellään on", (ekaLuku*ekaLuku))
 sade = int(input("Anna ympyrän säteen pituus kokonaislukuna: "))
-print("Ympyrän säde on", sade, "kehä on", )
+keha = 2 * PII * sade
+pintaAla = PII * (sade ** 2)
+print("Ympyrän säde on ", sade, ", kehä on ", keha, " ja pinta-ala on ", round(pintaAla, 14), ".", sep="")
+ekaLuku = int(input("Anna suorakulmion yhden sivun pituus kokonaislukuna: "))
+tokaLuku = int(input("Anna suorakulmion toisen sivun pituus kokonaislukuna: "))
+keha = ekaLuku * 2 + tokaLuku * 2
+pintaAla = ekaLuku * tokaLuku
+print("Suorakulmion sivut ovat ", ekaLuku," ja ", tokaLuku, "; kehä on ", keha, "; ja pinta-ala on ", pintaAla, ".", sep="")
+print("Kiitos ohjelman käytöstä.")
---
+++ L02-tehtavat/L02T5.py
@@ -0,0 +1,11 @@
+ekaLuku = int(input("Anna luku 0 ja 10 väliltä: "))
+tokaLuku = int(input("Anna toinen luku 0 ja 10 väliltä: "))
+kolmasLuku = int(input("Anna kolmas luku 0 ja 10 väliltä: "))
+
+summa = ekaLuku + tokaLuku + kolmasLuku
+keskiArvo = summa / 3
+print("\nAntamiesi lukujen summa on ", summa, ".", sep="")
+print("Antamiesi lukujen keskiarvo on ", keskiArvo, ".", sep="")
+print("Keskiarvo on kokonaislukuna ", int(keskiArvo), ".", sep="")
+print("Keskiarvo pyöristettynä 3 desimaalin tarkkuuteen on ", round(keskiArvo, 3), ".", sep="")
+print("Kiitos ohjelman käytöstä.")
