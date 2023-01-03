aac0f77849fadb0cd4a6d9fee5328d5cb5978f58
Jokke <joakim.ijas@gmail.com>
2023-01-03 11:31:43 +0200

    started L03T4

---
+++ L03-tehtavat/L03T4.py
@@ -0,0 +1,10 @@
+ekaSana = input("Anna sana 1: ")
+tokaSana = input("Anna sana 2: ")
+
+if (ekaSana < tokaSana):
+    print("'" + ekaSana + "' on aakkosissa aiemmin kuin '" + tokaSana + "'.")
+else: 
+    print("'" + tokaSana + "' on aakkosissa aiemmin kuin '" + ekaSana + "'.")
+
+
+print("Kiitos ohjelman käytöstä.")
