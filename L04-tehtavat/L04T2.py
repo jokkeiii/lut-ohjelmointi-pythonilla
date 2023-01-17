8130580b613e75e2a1807e54aa9246af28ab8c97
jokkeiii <joakim.ijas@gmail.com>
2023-01-17 08:41:37 +0200

    started L04T2

---
+++ L04-tehtavat/L04T2.py
@@ -0,0 +1,18 @@
+
+indeksi = 0
+arvosana = 0
+summa = 0
+while (arvosana != -1):
+    arvosana = int(input("Anna kurssiarvosana väliltä 1-5 (-1 lopettaa): "))
+    if (arvosana != -1 and (arvosana < 1 or arvosana > 5)):
+        print("Väärä syöte. Vain arvosanat 1-5 kelpaavat (-1 lopettaa).")
+        continue
+    summa += arvosana
+    indeksi += 1
+
+# keskiarvo ei jostain syysta toimi
+keskiarvo = summa / indeksi
+
+print("Arvosanojen keskiarvo on", round(keskiarvo, 2))
+
+print("Kiitos ohjelman käytöstä.")
