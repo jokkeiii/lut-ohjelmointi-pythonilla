be141fa83b46437309fd7698ac8e4087dd551c6f
Jokke <joakim.ijas@gmail.com>
2023-01-29 20:26:29 +0200

    updated template

--- malli.py
+++ malli.py
@@ -1,8 +1,20 @@
-# aloita kommentointi
+######################################################################
+# CT60A0203 Ohjelmoinnin perusteet
+# Tekijä: Joakim Ijäs
+# Opiskelijanumero: 001063376
+# Päivämäärä: xx/01/2023
+# Kurssin oppimateriaalien lisäksi työhön ovat vaikuttaneet seuraavat 
+# lähteet ja henkilöt, ja se näkyy tehtävässä seuraavalla tavalla:
+# 
+# Mahdollisen vilppiselvityksen varalta vakuutan, että olen tehnyt itse 
+# tämän tehtävän ja vain yllä mainitut henkilöt sekä lähteet ovat
+# vaikuttaneet siihen yllä mainituilla tavoilla.
+######################################################################
 
 def main():
     muuttuja = input("A")
     
+    # poistutaan paaohjelmasta
     return None
     
 # kutsutaan paaohjelmaa
