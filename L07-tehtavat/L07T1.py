2d03ffe98017c272306e68dc4951da65a142ff4c
Jokke <joakim.ijas@gmail.com>
2023-01-29 20:45:48 +0200

    finished L07T1

--- L07-tehtavat/L07T1.py
+++ L07-tehtavat/L07T1.py
@@ -3,47 +3,63 @@
 def main():
 
     # esitellaan lista
-    ostosLista = []
+    ostoslista = []
     
     # valikon valinta muuttuja
     valinta = '1'
 
+    # silmukka kunnes valinta on 0
     while (valinta != '0'):
-        valinta = valikko()
+
+        # kutsutaan aliohjelmaa
+        valinta = valikko(ostoslista)
         
+        # jos valinta on 1
         if (valinta == '1'):
+            # kysytaan syote ja lisataan se listaan
             tuote = input("Anna lisättävä tuote: ")
-            ostosLista.append(tuote)
-    
+            ostoslista.append(tuote)
+            print()
+        # jos valinta on 2
+        elif (valinta == '2'):
+            # tulostetaan tuotteiden maara
+            print("Ostoslistassasi on", len(ostoslista), "tuotetta.")
+            # kysytaan syote
+            poistoNumero = int(input("Anna poistettavan tuotteen järjestysnumero: "))
+            # poistetaan listasta kyseinen tuote 
+            del ostoslista[poistoNumero - 1]
+            print()
+        # jos valinta on 0
+        elif (valinta == '0'):
+            # tulostetaan listan sisalto
+            print("Sinulta jäi ostamatta seuraavat tuotteet:\n", ostoslista, sep="")
+
     # poistutaan paaohjelmasta
     return None
 
 
-def valikko():
+def valikko(ostoslista):
 
     # tulostetaan listan sisalto
-    print("Ostoslistasi sisältää seuraavat tuotteet:\n", ostosLista)
+    print("Ostoslistasi sisältää seuraavat tuotteet:\n", ostoslista, sep="")
     
     # tulostetaan valikkovaihtoehdot
     print("Voit valita alla olevista toiminnoista:\n1) Lisää\n2) Poista\n0) Lopeta")
-
-    # valikko silmukka
-    while (True):    
-        
-        # kysytaan syote
-        valinta = input("Valintasi: ")
-        
-        # jos syote on 1, 2 tai 0
-        if (valinta == '1' or valinta == '2' or valinta == '0'):
-            # poistutaan silmukasta
-            break
-        # 
-        else:
-            # tulostetaan virheviesti
-            print("Tunnistamaton valinta.")
+      
+    # kysytaan syote
+    valinta = input("Valintasi: ")
     
-    # palautetaan valinta
-    return valinta
+    # jos syote on 1, 2 tai 0
+    if (valinta == '1' or valinta == '2' or valinta == '0'):
+        # palautetaan valinta
+        return valinta
+    else:
+        # tulostetaan virheviesti
+        print("Tunnistamaton valinta.\n")
+        # palautetaan valinta
+        return valinta
+    
+    
 
 
 # kutsutaan paaohjelmaa
