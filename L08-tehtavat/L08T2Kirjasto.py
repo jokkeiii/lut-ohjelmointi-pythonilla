9fb038ad76274ad0e1746c6c8066f9460a443fd5
Jokke <joakim.ijas@gmail.com>
2023-02-15 12:38:05 +0200

    made library structure

---
+++ L08-tehtavat/L08T2Kirjasto.py
@@ -0,0 +1,45 @@
+######################################################################
+# CT60A0203 Ohjelmoinnin perusteet
+# Tekijä: Joakim Ijäs
+# Opiskelijanumero: 001063376
+# Päivämäärä: 15/01/2023
+# Kurssin oppimateriaalien lisäksi työhön ovat vaikuttaneet seuraavat 
+# lähteet ja henkilöt, ja se näkyy tehtävässä seuraavalla tavalla:
+# 
+# Mahdollisen vilppiselvityksen varalta vakuutan, että olen tehnyt itse 
+# tämän tehtävän ja vain yllä mainitut henkilöt sekä lähteet ovat
+# vaikuttaneet siihen yllä mainituilla tavoilla.
+######################################################################
+
+VERSIO = 1.0
+
+def CelsiusFahrenheit(muunnettava):
+    tulos = float(muunnettava)
+    # poistutaan paaohjelmasta
+    return tulos
+
+def FahrenheitCelsius(muunnettava):
+    tulos = float(muunnettava)
+    # poistutaan paaohjelmasta
+    return tulos
+
+def CelsiusKelvin(muunnettava):
+    tulos = float(muunnettava)
+    # poistutaan paaohjelmasta
+    return tulos
+
+def KelvinCelsius(muunnettava):
+    tulos = float(muunnettava)
+    # poistutaan paaohjelmasta
+    return tulos
+
+def KelvinFahrenheit(muunnettava):
+    tulos = float(muunnettava)
+    # poistutaan paaohjelmasta
+    return tulos
+
+def FahrenheitKelvin(muunnettava):
+    tulos = float(muunnettava)
+    # poistutaan paaohjelmasta
+    return tulos
+
