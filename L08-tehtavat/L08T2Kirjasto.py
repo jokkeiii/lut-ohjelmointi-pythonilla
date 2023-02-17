bc2a00ed8a1ba398056b40449fa13c921a50493d
Jokke <joakim.ijas@gmail.com>
2023-02-17 18:10:27 +0200

    finished functions

--- L08-tehtavat/L08T2Kirjasto.py
+++ L08-tehtavat/L08T2Kirjasto.py
@@ -2,7 +2,7 @@
 # CT60A0203 Ohjelmoinnin perusteet
 # Tekijä: Joakim Ijäs
 # Opiskelijanumero: 001063376
-# Päivämäärä: 15/01/2023
+# Päivämäärä: 17/01/2023
 # Kurssin oppimateriaalien lisäksi työhön ovat vaikuttaneet seuraavat 
 # lähteet ja henkilöt, ja se näkyy tehtävässä seuraavalla tavalla:
 # 
@@ -14,32 +14,31 @@
 VERSIO = 1.0
 
 def CelsiusFahrenheit(muunnettava):
-    tulos = float(muunnettava)
+    tulos = float(muunnettava * 1.8 + 32)
     # poistutaan paaohjelmasta
     return tulos
 
 def FahrenheitCelsius(muunnettava):
-    tulos = float(muunnettava)
+    tulos = float((muunnettava - 32) * (5 / 9))
     # poistutaan paaohjelmasta
     return tulos
 
 def CelsiusKelvin(muunnettava):
-    tulos = float(muunnettava)
+    tulos = float(muunnettava + 273.15)
     # poistutaan paaohjelmasta
     return tulos
 
 def KelvinCelsius(muunnettava):
-    tulos = float(muunnettava)
+    tulos = float(muunnettava - 273.15)
     # poistutaan paaohjelmasta
     return tulos
 
 def KelvinFahrenheit(muunnettava):
-    tulos = float(muunnettava)
+    tulos = float(1.8 * (muunnettava - 273.15) + 32)
     # poistutaan paaohjelmasta
     return tulos
 
 def FahrenheitKelvin(muunnettava):
-    tulos = float(muunnettava)
+    tulos = float((5 / 9) * (muunnettava -32) + 273.15)
     # poistutaan paaohjelmasta
     return tulos
-
